c79dc328fbc7ef99d65901bd59542dec73cc9625
Lionel Savidan <l.savidan.ubiqum@gmail.com>
2020-07-25 14:00:01 +0200

    added client folder - deployed

---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Toasted from 'vue-toasted';
+
+
+Vue.config.productionTip = false
+
+// Load Toast Plugin
+
+Vue.use(Toasted, {
+  iconPack: 'fontawesome'
+});
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
